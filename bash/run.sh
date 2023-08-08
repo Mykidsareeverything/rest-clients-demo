f0e5f672fd7ba527a217c2f4251f5b1377f355de
Mykidsareeverything <129775315+Mykidsareeverything@users.noreply.github.com>
2023-08-08 07:13:32 -0400

    Update run.sh

--- bash/run.sh
+++ bash/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Configuration
-ORG="---"
-KEY="---"
-SEC="---"
+ORG="---" 520f7246-d6a4-463e-9688-72db1a2c741e
+KEY="---"25ef6083-e791-4bad-a0c0-462a1a1220e3
+SEC="---" 4720b3e3-7901-4468-91a1-3474c41513de
 #API="https://api2.nicehash.com" #prod env
 API="https://api-test.nicehash.com" # test env
 
